b3004d06c1d525d269a6cd5faff6e0fc297484d0
USER <USER@DESKTOP-AU69HNR>
2026-03-30 15:19:49 -0500

    retoques para el juego

--- src/proyectoprogramacion3/Tablero.java
+++ src/proyectoprogramacion3/Tablero.java
@@ -1,107 +1,67 @@
-
 package proyectoprogramacion3;
 
 public class Tablero {
-    private int ancho;
-    private int alto;
+    private int ancho, alto, puntaje;
     private Serpiente serpiente;
     private Comida comida;
     private String estado;
-    private int puntaje;
 
     public Tablero(int ancho, int alto) {
         this.ancho = ancho;
         this.alto = alto;
-        serpiente = new Serpiente();
-        comida = new Comida();
-        comida.generar(ancho, alto, serpiente);
-        estado = "Jugando";
-        puntaje = 0;
+        this.serpiente = new Serpiente();
+        this.comida = new Comida();
+        this.comida.generar(ancho, alto, serpiente);
+        this.estado = "Jugando";
+        this.puntaje = 0;
     }
 
     public void actualizar() {
-
         boolean comio = verificarComida();
-
         serpiente.mover(comio);
-
         if (comio) {
             puntaje += 10;
             comida.generar(ancho, alto, serpiente);
         }
-
-        if (verificarColision()) {
-            estado = "Perdiste";
-        }
+        if (verificarColision()) estado = "Perdiste";
     }
 
     public boolean verificarComida() {
         Nodo cabeza = serpiente.getCabeza();
-        return cabeza.getX() == comida.getX() &&
-               cabeza.getY() == comida.getY();
+        return cabeza.getX() == comida.getX() && cabeza.getY() == comida.getY();
     }
 
     public boolean verificarColision() {
-
         Nodo cabeza = serpiente.getCabeza();
-
-        if (cabeza.getX() < 0 || cabeza.getX() >= ancho ||
-            cabeza.getY() < 0 || cabeza.getY() >= alto) {
-            return true;
-        }
-
+        if (cabeza.getX() < 0 || cabeza.getX() >= ancho || cabeza.getY() < 0 || cabeza.getY() >= alto) return true;
         return serpiente.colisionPropia();
     }
 
     public String dibujar() {
-
-        String texto = "";
-
+        String mapa = "";
         for (int i = 0; i < alto; i++) {
             for (int j = 0; j < ancho; j++) {
-
-                boolean esCuerpo = false;
-                Nodo actual = serpiente.getCuerpo().getFrente();
-
-                while (actual != null) {
-
-                    if (actual.getX() == j && actual.getY() == i) {
-
-                        if (actual == serpiente.getCabeza()) {
-                            texto += "O ";
-                        } else {
-                            texto += "S ";
-                        }
-
-                        esCuerpo = true;
+                boolean serp = false;
+                Nodo act = serpiente.getCuerpo().getFrente();
+                while (act != null) {
+                    if (act.getX() == j && act.getY() == i) {
+                        mapa += "S ";
+                        serp = true;
                         break;
                     }
-
-                    actual = actual.getSiguiente();
+                    act = act.getSiguiente();
                 }
-
-                if (!esCuerpo) {
-                    if (j == comida.getX() && i == comida.getY()) {
-                        texto += "C ";
-                    } else {
-                        texto += ". ";
-                    }
+                if (!serp) {
+                    if (comida.getX() == j && comida.getY() == i) mapa += "C ";
+                    else mapa += ". ";
                 }
             }
-            texto += "\n";
+            mapa += "\n";
         }
-
-        texto += "Estado: " + estado;
-        texto += "\nPuntaje: " + puntaje;
-
-        return texto;
+        return mapa;
     }
 
-    public String getEstado() {
-        return estado;
-    }
-
-    public Serpiente getSerpiente() {
-        return serpiente;
-    }
+    public int getPuntaje() { return puntaje; }
+    public String getEstado() { return estado; }
+    public Serpiente getSerpiente() { return serpiente; }
 }

c4d2d943dea4aaf44484545e2e90a3545a2b68d8
USER <USER@DESKTOP-AU69HNR>
2026-03-30 11:17:26 -0500

    Mejora en generación de comida con mejora en posición

--- src/proyectoprogramacion3/Comida.java
+++ src/proyectoprogramacion3/Comida.java
@@ -1,24 +1,39 @@
 
 package proyectoprogramacion3;
 
-
 public class Comida {
     private int x;
     private int y;
 
     public Comida() {
-        generar(10, 10);
+        generar(10, 10, null);
     }
 
-    public void generar(int ancho, int alto) {
-        x = (int)(Math.random() * ancho);
-        y = (int)(Math.random() * alto);
+    public void generar(int ancho, int alto, Serpiente serpiente) {
+
+        boolean valido = false;
+
+        while (!valido) {
+
+            x = (int)(Math.random() * ancho);
+            y = (int)(Math.random() * alto);
+
+            valido = true;
+
+            if (serpiente != null) {
+                Nodo actual = serpiente.getCuerpo().getFrente();
+
+                while (actual != null) {
+                    if (actual.getX() == x && actual.getY() == y) {
+                        valido = false;
+                        break;
+                    }
+                    actual = actual.getSiguiente();
+                }
+            }
+        }
     }
 
-    public int getX() { 
-        return x; 
-    }
-    public int getY() { 
-        return y; 
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
 }

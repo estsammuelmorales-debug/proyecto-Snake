8154a1b3d566dd9c3a9ac930e4fbbe825ab1367e
USER <USER@DESKTOP-AU69HNR>
2026-03-30 15:34:23 -0500

    clase serpiente ultima configuracion y cambio

--- src/proyectoprogramacion3/Serpiente.java
+++ src/proyectoprogramacion3/Serpiente.java
@@ -1,4 +1,3 @@
-
 package proyectoprogramacion3;
 
 public class Serpiente {
@@ -8,50 +7,50 @@ public class Serpiente {
     public Serpiente() {
         cuerpo = new Cola();
         direccion = 'D';
-
+        
         cuerpo.encolar(3, 5);
         cuerpo.encolar(4, 5);
         cuerpo.encolar(5, 5);
     }
 
-    public void mover(boolean crecer) {
-
+    public void mover(boolean comio) {
         Nodo cabeza = cuerpo.getFin();
         int x = cabeza.getX();
         int y = cabeza.getY();
 
         if (direccion == 'D') x++;
-        else if (direccion == 'I') x--;
-        else if (direccion == 'U') y--;
-        else if (direccion == 'A') y++;
+        else if (direccion == 'A') x--;
+        else if (direccion == 'W') y--;
+        else if (direccion == 'S') y++;
 
         cuerpo.encolar(x, y);
 
-        if (!crecer) {
-            cuerpo.desencolar();
+        if (!comio) {
+        cuerpo.desencolar();
         }
     }
 
-    public void cambiarDireccion(char d) {
-        direccion = d;
+    public void cambiarDireccion(char nuevaDir) {
+        if (nuevaDir == 'W' && direccion != 'S') direccion = 'W';
+        else if (nuevaDir == 'S' && direccion != 'W') direccion = 'S';
+        else if (nuevaDir == 'A' && direccion != 'D') direccion = 'A';
+        else if (nuevaDir == 'D' && direccion != 'A') direccion = 'D';
     }
 
-    public Nodo getCabeza() {
-        return cuerpo.getFin();
+    public void crecer() {
+        Nodo cabeza = cuerpo.getFin();
+        cuerpo.encolar(cabeza.getX(), cabeza.getY());
     }
 
-    public Cola getCuerpo() {
-        return cuerpo;
-    }
-
+    public Cola getCuerpo() { return cuerpo; }
+    public Nodo getCabeza() { return cuerpo.getFin(); }
+    
     public boolean colisionPropia() {
-
         Nodo cabeza = cuerpo.getFin();
         Nodo actual = cuerpo.getFrente();
-
+        
         while (actual != null && actual != cabeza) {
-            if (actual.getX() == cabeza.getX() &&
-                actual.getY() == cabeza.getY()) {
+            if (actual.getX() == cabeza.getX() && actual.getY() == cabeza.getY()) {
                 return true;
             }
             actual = actual.getSiguiente();

d0ed878a5e81c0738f41d96094047f8051b6ca4c
USER <USER@DESKTOP-AU69HNR>
2026-04-09 16:55:41 -0500

    descargue la libreria del profesor y se adapto la clase serpiente y se eliminaron las demas clases

--- src/proyectoprogramacion3/Serpiente.java
+++ src/proyectoprogramacion3/Serpiente.java
@@ -1,61 +1,31 @@
 package proyectoprogramacion3;
 
 public class Serpiente {
-    private Cola cuerpo;
-    private char direccion;
+    private String jugador;
+    private int puntaje;
 
     public Serpiente() {
-        cuerpo = new Cola();
-        direccion = 'D';
-        
-        cuerpo.encolar(3, 5);
-        cuerpo.encolar(4, 5);
-        cuerpo.encolar(5, 5);
+        jugador = "Anonimo";
+        puntaje = 0;
     }
 
-    public void mover(boolean comio) {
-        Nodo cabeza = cuerpo.getFin();
-        int x = cabeza.getX();
-        int y = cabeza.getY();
-
-        if (direccion == 'D') x++;
-        else if (direccion == 'A') x--;
-        else if (direccion == 'W') y--;
-        else if (direccion == 'S') y++;
-
-        cuerpo.encolar(x, y);
-
-        if (!comio) {
-        cuerpo.desencolar();
-        }
+    public void iniciarJuego() {
+        puntaje = (int)(Math.random() * 100);
     }
 
-    public void cambiarDireccion(char nuevaDir) {
-        if (nuevaDir == 'W' && direccion != 'S') direccion = 'W';
-        else if (nuevaDir == 'S' && direccion != 'W') direccion = 'S';
-        else if (nuevaDir == 'A' && direccion != 'D') direccion = 'A';
-        else if (nuevaDir == 'D' && direccion != 'A') direccion = 'D';
+    public int getPuntaje() {
+        return puntaje;
     }
 
-    public void crecer() {
-        Nodo cabeza = cuerpo.getFin();
-        cuerpo.encolar(cabeza.getX(), cabeza.getY());
+    public String getJugador() {
+        return jugador;
     }
 
-    public Cola getCuerpo() { return cuerpo; }
-    public Nodo getCabeza() { return cuerpo.getFin(); }
-    
-    public boolean colisionPropia() {
-        Nodo cabeza = cuerpo.getFin();
-        Nodo actual = cuerpo.getFrente();
-        
-        while (actual != null && actual != cabeza) {
-            if (actual.getX() == cabeza.getX() && actual.getY() == cabeza.getY()) {
-                return true;
-            }
-            actual = actual.getSiguiente();
-        }
-        return false;
+    public void setJugador(String jugador) {
+        this.jugador = jugador;
+    }
+
+    public String toString() {
+        return "Jugador: " + jugador + " | Puntaje: " + puntaje;
     }
 }
-

f92602e065d0428ee784df4c239d10a67586c6bf
USER <USER@DESKTOP-AU69HNR>
2026-04-14 00:40:19 -0500

    ultima edicion de snake respecto a la libreria

---
+++ src/proyectoprogramacion3/Posicion.java
@@ -0,0 +1,13 @@
+
+package proyectoprogramacion3;
+
+
+public class Posicion {
+    public int x;
+    public int y;
+
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
